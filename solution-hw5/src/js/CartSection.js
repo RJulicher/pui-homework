67e8aafeb472a9b15dc2b417d5dd3675930cd21d
RJulicher <rjuliche@andrew.cmu.edu>
2023-10-08 16:12:12 -0400

    Final styling

--- solution-hw5/src/js/CartSection.js
+++ solution-hw5/src/js/CartSection.js
@@ -12,24 +12,26 @@ class CartSection extends Component {
     return (
       this.props.active && 
       <section className="cartSection">
-        <div className="cartText">
-          <span className="cartText" id="cartHeader">
-            {"Shopping Cart ("+numItems+(numItems !== 1 ? " items)":" item)")}
-          </span>
-
-          <span className="cartText" id="cartTotal">
-            {"Total: $ " + this.props.totalCost}
-          </span>
-        </div>
         
         {this.props.cartContent.length > 0 ?
-          this.props.cartContent.map((product, idx) => {
-            //console.log(roll.type);
-            return <CartProduct key={idx} roll={product} id={idx}
-                                removeCartItem={this.removeCartItem}/>
-          })
+          <div>
+            <div className="cartText">
+              <span className="cartText" id="cartHeader">
+                {"Shopping Cart ("+numItems+(numItems !== 1 ? " items)":" item)")}
+              </span>
+
+              <span className="cartText" id="cartTotal">
+                {"Total: $ " + this.props.totalCost}
+              </span>
+            </div>
+            {this.props.cartContent.map((product, idx) => {
+              //console.log(roll.type);
+              return <CartProduct key={idx} roll={product} id={idx}
+                                  removeCartItem={this.removeCartItem}/>
+            })}
+          </div>
           :
-          <h3>The cart is empty!</h3>}
+          <h3 id="emptyCart">The cart is empty!</h3>}
       </section>
     );
   }
